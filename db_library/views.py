3e4640623db63d12cf81c17a5486212291076f2c
Amerohful <semb1996@gmail.com>
2019-11-25 11:25:17 +0800

    add form for tickets

--- db_library/views.py
+++ db_library/views.py
@@ -1,19 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 from .models import Section, Reader, Book, Ticket
 
 
-def home(requests):
-    return render(requests, 'home.html')
+def home(request):
+    return render(request, 'home.html')
 
-def sections(requests):
-    return render(requests, 'sections.html', {'sections': Section.objects.all()})
+def sections(request):
+    return render(request, 'sections.html', {'sections': Section.objects.all()})
 
-def books(requests):
-    return render(requests, 'books.html', {'books': Book.objects.all()})
+def books(request):
+    return render(request, 'books.html', {'books': Book.objects.all()})
 
-def tickets(requests):
-    return render(requests, 'tickets.html', {'tickets': Ticket.objects.all()})
+@csrf_protect
+def tickets(request):
+    if request.method == 'POST':
+        reader_id = request.POST['reader_id']
+        book_id = request.POST['book_id']
+        issue_date = request.POST['issue_date']
+        return_date = request.POST['return_date']
+        Ticket(reader_id=Reader.objects.get(pk=reader_id), book_id=Book.objects.get(pk=book_id),
+               issue_date=issue_date, return_date=return_date).save()
+        return render(request, 'tickets.html', {
+            'tickets': Ticket.objects.all(),
+            'readers': Reader.objects.all(),
+            'books': Book.objects.all(),
+        })
+    else:
+        return render(request, 'tickets.html', {
+            'tickets': Ticket.objects.all(),
+            'readers': Reader.objects.all(),
+            'books': Book.objects.all(),
+        })
 
-def readers(requests):
-    return render(requests, 'readers.html', {'readers': Reader.objects.all()})
+def readers(request):
+    return render(request, 'readers.html', {'readers': Reader.objects.all()})
+
+def report_about_count_book(request):
+    pass
+
+def report_about_reading_book(request):
+    pass
